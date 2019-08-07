07c9905e90b066ce7d5fa8fdb292b5a0cfc942f5
Eric YU <leyu@ixiacom.com>
2019-08-07 23:21:20 +0800

    create python script to train

---
+++ navigation.py
@@ -0,0 +1,68 @@
+import sys
+sys.path.append("./python/")
+
+from unityagents import UnityEnvironment
+from agent import *
+
+
+agent = Agent(state_size=37, action_size=4, seed=0)
+
+# interact with environment
+env = UnityEnvironment(file_name="./Banana_Windows_x86_64/Banana.exe")
+#-- get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+def train(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+
+
+    for i_episode in range(1, n_episodes+1):
+        
+        env_info = env.reset(train_mode=True)[brain_name]
+        state = env_info.vector_observations[0]
+        score = 0
+        for t in range(max_t):
+            
+            action = agent.act(state, eps)
+            #print('t:',t,'action:', action)
+            
+            env_info = env.step([action])[brain_name]
+            next_state = env_info.vector_observations[0]   # get the next state
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0]                  # see if episode has finished
+            
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break 
+                
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+            if np.mean(scores_window)>=14.0:
+                print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+                torch.save(agent.qnetwork_local.state_dict(), 'model.pth')
+                break
+    return scores
+
+# scores = train(n_episodes=20, max_t=10)
+scores = train()
+
+env.close()
